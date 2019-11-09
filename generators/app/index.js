bcb38919c9906f1854e89ce4e3064d22d7f6ac9d
Nicole Mealey <nicole.ewert22@gmail.com>
2019-11-08 19:37:18 -0700

    Initial project structure

---
+++ generators/app/index.js
@@ -0,0 +1,11 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    constructor(args, opts) {
+        super(args, opts);
+    }
+
+    hello() {
+        this.log('hello, world')
+    }
+};
